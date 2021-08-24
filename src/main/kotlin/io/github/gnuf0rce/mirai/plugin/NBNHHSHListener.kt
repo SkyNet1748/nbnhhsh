887ff82c8f0368bb7c4d4f13dd234e0b680a8ab3
cssxsh <cssxsh@gmail.com>
2021-08-24 11:51:13 +0800

    feat: isEmpty

--- src/main/kotlin/io/github/gnuf0rce/mirai/plugin/NBNHHSHListener.kt
+++ src/main/kotlin/io/github/gnuf0rce/mirai/plugin/NBNHHSHListener.kt
@@ -14,21 +14,22 @@ object NBNHHSHListener : CoroutineScope {
     override lateinit var coroutineContext: CoroutineContext
 
     @OptIn(ConsoleExperimentalApi::class)
-    fun start(parent: CoroutineScope = GlobalScope, regex: Regex = """[a-z]+""".toRegex()) {
+    fun start(
+        parent: CoroutineScope = GlobalScope,
+        regex: Regex = """[a-z]+""".toRegex(),
+        exclude: Set<String> = emptySet()
+    ) {
         coroutineContext = parent.childScopeContext("NBNHHSHListener")
         globalEventChannel().subscribeMessages {
             regex findingReply {
                 if (HHSHCommand.permission.testPermission(toCommandSender()).not()) return@findingReply null
-                val words = regex.findAll(message.contentToString()).map { result -> result.value }
+                val words = regex.findAll(message.contentToString()).map { result -> result.value }.toList() - exclude
+                if (words.isEmpty()) return@findingReply null
+                val tran = hhsh(words).filter { it.trans.isNotEmpty() }
+                if (tran.isEmpty()) return@findingReply null
 
-                val tran = hhsh(words.toList()).filter { it.trans.isNotEmpty() }
-
-                if (tran.isNotEmpty()) {
-                    message.quote() + hhsh(words.toList()).joinToString("\n", "好好说话: ") { item ->
-                        "${item.name} -> ${item.trans}"
-                    }
-                } else {
-                    null
+                message.quote() + hhsh(words.toList()).joinToString("\n", "好好说话: ") { item ->
+                    "${item.name} -> ${item.trans}"
                 }
             }
         }
